03bd943a3a2f54447c965c7a4e289ed514e0e1d8
ycuervob <ycuervob@unal.edu.co>
2022-09-29 15:30:13 -0500

    solved bad request

--- utilities/postcheck.js
+++ utilities/postcheck.js
@@ -11,11 +11,17 @@ async function postFunction(req, res, next) {
     const insertText_1 = 'INSERT INTO public."logs" ("postlogs") VALUES($1);';
     const res_1 = await db.query(insertText_1, [req.body]);
 
+    if (!req.body?.lista) {
+        res.statusCode = 400;
+        return res.send({});
+    }
+
+
     const [id_device, bateria, temperatura, humedad, flat, flon, timestamp, numero_satelites, varianza] = req.body?.lista;
     const data = [id_device, bateria, temperatura, humedad, flat, flon, timestamp, numero_satelites, varianza];
 
     if (data?.every(element => !(element == null)) == true) {
-    
+
         const insertText = 'CALL public.insertarlectura($1,$2,$3,$4,$5,$6,$7,$8,$9);';
         const res_db = await db.query(insertText, [id_device, bateria, temperatura, humedad, flat, flon, timestamp, numero_satelites, varianza]);
         res.statusCode = res_db?.command ? 200 : 503;
